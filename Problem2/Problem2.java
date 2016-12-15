75d7d601dda1b370f90ac9a8b66869d35b81e59e
Golden-Dragon <snehosurvey2293@gmail.com>
2016-12-15 19:49:16 +0530

    Add files via upload

---
+++ Problem2/Problem2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Problem2
+{
+	public static void main(String args[]){
+		int a=1,b=1,c,s=0;
+		c=a+b;
+		while(c<4000000){
+		  a=b;
+		  b=c;
+		  if(c%2==0){
+			s=s+c;  
+		  }
+		  c=a+b;
+		}
+		System.out.println("The sum is "+s);
+	}
+
+}
