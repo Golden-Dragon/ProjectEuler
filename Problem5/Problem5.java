f86c5b60a6715fa94009c4c4558e24a665e58734
Golden-Dragon <snehosurvey2293@gmail.com>
2016-12-16 17:16:16 +0530

    Add files via upload

---
+++ Problem5/Problem5.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class Problem5{
+	
+	public static void main(String args[]){
+		long n,i=1,j,k=21,c=0;
+		while(k>0){
+			for(i=20;i>=1;i--){
+				if(k%i==0)
+					c++;
+			}	
+			if(c==20)
+			   break;
+			else{
+		      k++;
+			  c=0;
+			}
+		}
+		System.out.println("Smallest number divisible by the first 20 natural numbers is "+k);
+	}
+	
+}
