07288d152371948dce01e7e5b416f4b70911b377
Golden-Dragon <snehosurvey2293@gmail.com>
2016-12-16 16:51:31 +0530

    Add files via upload

---
+++ Problem4/Problem4.java
@@ -0,0 +1,30 @@
+import java.io.*;
+class Problem4{
+
+    public static boolean CheckPalindrome(long n){
+		long r=0,d,or=n;
+		while(n>0){
+			d=n%10;
+			n=n/10;
+			r=(r*10)+d;
+		}
+		if(r==or)
+			return true;
+		else
+			return false;
+	}
+	
+	public static void main(String args[]){
+		long i,j,p=1,max=1;
+		boolean cp;
+		outer: for(i=100;i<1000;i++)
+			for(j=100;j<1000;j++){
+				p=i*j;
+				cp=CheckPalindrome(p);
+				if((cp==true) && (p>max))
+					max=p;
+			}
+		System.out.println("The largest multiple is "+max);
+	}
+
+}
